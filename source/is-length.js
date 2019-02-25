0dbe7514d88de42831829aef8de86de8154b3e31
itper <itperchen@gmail.com>
2019-02-25 11:26:25 +0800

    Update is-length.js
    support rpx

--- source/is-length.js
+++ source/is-length.js
@@ -1,4 +1,4 @@
-var LENGTH = /^(\+|-)?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/i;
+var LENGTH = /^(\+|-)?([0-9]*\.)?[0-9]+(em|ex|rpx|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/i;
 var ZERO = /^(\+|-)?(0*\.)?0+$/;
 
 module.exports = function(value) {
